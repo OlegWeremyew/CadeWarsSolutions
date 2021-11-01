f05eff6a18af43ca7667107dbc89101372123c98
OlegWeremyew <olegweremey1994@mail.ru>
2021-11-01 08:08:57 +0300

    Solution

--- Sentence Smash.js
+++ Sentence Smash.js
@@ -1,3 +1,8 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+*/
 function isPalindrome(line) {
     let str = line.toString();
     let reverse = str.split("").reverse().join('');
