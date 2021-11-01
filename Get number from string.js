07be26d5161cfdbe4f32ce7280cbc4cc552837c9
olegweremyew <olegweremey1994@mail.ru>
2021-11-01 17:36:23 +0300

    Solution

--- Get number from string.js
+++ Get number from string.js
@@ -1,5 +1,5 @@
 /*
-
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
 */
 function getNumberFromString(s) {
     let num = s.replace(/[^+\d]/g, '');
