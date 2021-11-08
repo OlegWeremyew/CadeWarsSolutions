34191e662c3643a96b3ae733b0aeeec842ca0c12
olegweremyew <olegweremey1994@mail.ru>
2021-11-08 22:25:51 +0300

    Solution

--- 7kyu/sPoNgEbOb MeMe.js
+++ 7kyu/sPoNgEbOb MeMe.js
@@ -11,3 +11,12 @@ function spongeMeme(sentence) {
     }
     return arr.join('');
 }
+
+/*
+You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+
+Examples:
+
+spongeMeme("stop Making spongebob Memes!") // => 'StOp mAkInG SpOnGeBoB MeMeS!'
+spongeMeme("colored teens cant Be successful in tech") // =>'CoLoReD TeEnS CaNt bE
+*/
