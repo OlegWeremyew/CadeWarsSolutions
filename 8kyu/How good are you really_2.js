9fda71a5d3898c4269b05151aea61c79ffbfe270
olegweremyew <olegweremey1994@mail.ru>
2021-12-01 21:46:19 +0300

    Solution

--- 8kyu/How good are you really_2.js
+++ 8kyu/How good are you really_2.js
@@ -9,3 +9,7 @@ Return True if you're better, else False!
 Note:
 Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
  */
+
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+}
