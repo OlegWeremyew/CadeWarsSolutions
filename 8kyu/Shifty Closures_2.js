65fdc7053edb6d7c598ac7d5b16878a845fa8b00
olegweremyew <olegweremey1994@mail.ru>
2021-12-20 22:39:37 +0300

    second solution

---
+++ 8kyu/Shifty Closures_2.js
@@ -0,0 +1,18 @@
+/*
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greet_abe() actually greet Abe?
+ */
+
+var name;
+
+function greet_abe() {
+    name = 'Abe';
+    return "Hello, " + name + '!';
+};
+
+
+function greet_ben() {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+};
