064f3023adb7bcc2e5042fc7713bef66f49e5ebf
olegweremyew <olegweremey1994@mail.ru>
2021-12-20 22:40:19 +0300

    refactor

--- 8kyu/Shifty Closures_2.js
+++ 8kyu/Shifty Closures_2.js
@@ -5,14 +5,12 @@ Why doesn't greet_abe() actually greet Abe?
  */
 
 var name;
-
 function greet_abe() {
     name = 'Abe';
     return "Hello, " + name + '!';
-};
-
+}
 
 function greet_ben() {
     name = 'Ben';
     return "Hello, " + name + '!';
-};
+}
