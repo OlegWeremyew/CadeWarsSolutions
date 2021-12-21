d2e5cd9bf07984dfb135ac9aaacdd18c788a3f11
olegweremyew <olegweremey1994@mail.ru>
2021-12-21 21:27:52 +0300

    refactor

---
+++ 8kyu/Return to Sanity.js
@@ -0,0 +1,12 @@
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+ */
+
+let mystery = () => {
+    var results = {
+        sanity: 'Hello'
+    }
+    return results;
+}
+
+console.log(mystery())
