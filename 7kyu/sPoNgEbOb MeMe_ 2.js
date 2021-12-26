de931182505370f4e9a5d10cbf9cbcd672194281
olegweremyew <olegweremey1994@mail.ru>
2021-12-26 20:01:34 +0300

    Solution

--- 7kyu/sPoNgEbOb MeMe_ 2.js
+++ 7kyu/sPoNgEbOb MeMe_ 2.js
@@ -1,8 +1,9 @@
 /*
 Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?*/
 
-
-
+function spongeMeme(sentence) {
+    return sentence.split('').map((v, i) => i % 2 ? v.toLowerCase() : v.toUpperCase()).join('');
+}
 
 /*
 You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
