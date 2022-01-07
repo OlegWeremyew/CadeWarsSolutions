30b185791e48ffd8aafc35ea6a55758583c1b473
olegweremyew <olegweremey1994@mail.ru>
2022-01-07 20:26:16 +0300

    Solution

--- 6ku/Stop gninnipS My sdroW.js
+++ 6ku/Stop gninnipS My sdroW.js
@@ -3,3 +3,12 @@ Write a function that takes in a string of one or more words, and returns the sa
 
 Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
  */
+
+function spinWords(string) {
+    return string
+        .split(" ")
+        .map(m => m.length >= 5 ? m.split("").reverse().join("") : m)
+        .join(" ")
+}
+
+console.log(spinWords("hey fellow warriors"))
