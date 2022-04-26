586bd2a56d9e122b92bbda18d41fa130308644fa
olegweremyew <olegweremey1994@mail.ru>
2022-04-26 23:56:24 +0300

    1

--- 8kyu/A Needle in the Haystack.js
+++ 8kyu/A Needle in the Haystack.js
@@ -13,7 +13,7 @@ should return "found the needle at position 5"
 function findNeedle(haystack) {
     for (let i = 0; i < haystack.length; i++) {
         if (haystack[i] === "needle") {
-            return 'found the needle at position ' + i;
+            return 'found the needle at position ' + i
         }
     }
 }
